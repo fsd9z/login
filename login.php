de7144fca02046d944f501bf3ad7913f11fce940
fsd9z <97798629+fsd9z@users.noreply.github.com>
2023-12-29 19:05:10 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
+    // Perform your validation and authentication here
+    // For demo purposes, let's check if the username and password are "demo" and "password"
+
+    if ($username === "demo" && $password === "password") {
+        // Successful login
+        echo "Login successful! Welcome, " . $username;
+    } else {
+        // Invalid credentials
+        echo "Invalid username or password. Please try again.";
+    }
+}
+?>
